c95a26450f5bdf0580d32f4477b39d81c90f2d6f
Diep Pham <diep@kyber.network>
2018-11-09 16:24:10 +0700

    improve build script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-cp -R /slate/temp/* / /slate/source/
-bundle exec middleman build --clean 
+set -euo pipefail
+
+cp -R /slate /slate-tmp
+cp -R /template/* /slate-tmp/source/
+cd /slate-tmp/source || exit 1
+bundle exec middleman build --clean
+rm -rf /slate/build/*
+cp -R /slate-tmp/build/* /slate/build/
+rm -rf /slate-tmp
